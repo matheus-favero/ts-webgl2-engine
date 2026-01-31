a433fe07955eb77342231f4dfb6e65feea329534
Matheus Favero <mathfaveamorime@gmail.com>
2026-01-31 11:42:17 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,18 @@
+const canvas = document.querySelector('canvas');
+const gl = canvas?.getContext('webgl2');
+
+if(!gl){
+    console.error("Webgl2 is not supported by this browser.");
+}
+else{
+    try{
+        main();
+    }catch(e){
+        console.error("Error while compiling: ", e);
+    }
+}
+
+function main(){
+    
+
+}
