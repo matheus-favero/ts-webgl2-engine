ad5342c0e3647a480cf4ac4914043b9efef67d9f
Matheus Favero <mathfaveamorime@gmail.com>
2026-01-31 22:34:24 -0300

    need to create vertex array

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,78 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function createShader(
+  gl: WebGL2RenderingContext,
+  type: number,
+  script: string,
+) {
+  const shader = gl.createShader(type);
+  if (!shader) {
+    console.log("Couldn't load shader");
+    return;
+  }
+  gl.shaderSource(shader, script);
+  gl.compileShader(shader);
+  const statusSuccess = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (statusSuccess) {
+    return shader;
+  }
+  console.error("Error while compiling shader: ", gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function createProgram(
+  gl: WebGL2RenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader,
+) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  const statusSuccess = gl.getProgramParameter(program, gl.COMPILE_STATUS);
+  if (statusSuccess) {
+    return program;
+  }
+  console.error(
+    "Error while compiling shader: ",
+    gl.getProgramInfoLog(program),
+  );
+  gl.deleteProgram(program);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function main() {
+  const canvas = document.querySelector("canvas");
+  const gl = canvas?.getContext("webgl2");
+
+  if (!gl) {
+    alert("Your browser doesn't support webgl2!");
+    return;
+  }
+
+  const vertexScript = await fetch("./shaders/vertex.glsl");
+  const fragmentScript = await fetch("./shaders/fragment.glsl");
+
+  const vertexShader = createShader(
+    gl,
+    gl.VERTEX_SHADER,
+    await vertexScript.text(),
+  );
+  const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    await fragmentScript.text(),
+  );
+
+  if (!vertexShader || !fragmentShader) {
+    console.error("No vertex or fragment shader");
+    return;
+  }
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
+
+  const vao = gl.createVertexArray();
+}
+
+try {
+  main();
+} catch (e) {
+  console.error(e);
+}
---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,5 @@
+# version 300 es
+out vec4 color;
+void main(){
+    color = vec4(0.9, 1, 0.3, 1);
+}
