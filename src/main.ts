cdc15c37e847db86ce8dcd4bba5fa14d24d4a3ee
Matheus Favero <mathfaveamorime@gmail.com>
2026-03-18 23:28:10 -0300

    switched to astro, added 3d configs and transformation class

---
+++ src/main.ts
@@ -0,0 +1,107 @@
+function createShader(
+  gl: WebGL2RenderingContext,
+  type: number,
+  script: string,
+) {
+  const shader = gl.createShader(type);
+  if (!shader) {
+    console.log("Couldn't load shader");
+    return;
+  }
+
+  gl.shaderSource(shader, script);
+
+  gl.compileShader(shader);
+  const statusSuccess = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (statusSuccess) {
+    return shader;
+  }
+
+  console.error(`Error while compiling shader:\n${script}\n${gl.getShaderInfoLog(shader)}`);
+  gl.deleteShader(shader);
+}
+
+function createProgram(
+  gl: WebGL2RenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader,
+) {
+  const program = gl.createProgram();
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  gl.linkProgram(program);
+
+  const statusSuccess = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (statusSuccess) {
+    return program;
+  }
+
+  console.error(
+    "Error while compiling program: ",
+    gl.getProgramInfoLog(program),
+  );
+  gl.deleteProgram(program);
+}
+
+function render(
+  gl: WebGL2RenderingContext,
+  vao: WebGLVertexArrayObject,
+  program: WebGLProgram,
+) {
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(program);
+
+  gl.bindVertexArray(vao);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+  window.requestAnimationFrame(() => render(gl, vao, program));
+}
+
+async function main() {
+  const canvas = document.querySelector("canvas");
+  const gl = canvas?.getContext("webgl2");
+
+  if (!gl) {
+    alert("Your browser doesn't support webgl2!");
+    return;
+  }
+
+  const vertexScript = await fetch("shaders/vertex.glsl");
+  const fragmentScript = await fetch("shaders/fragment.glsl");
+
+  const vertexShader = createShader(
+    gl,
+    gl.VERTEX_SHADER,
+    await vertexScript.text(),
+  );
+  const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    await fragmentScript.text(),
+  );
+
+  if (!vertexShader || !fragmentShader) {
+    console.error("No vertex or fragment shader");
+    return;
+  }
+
+  const program = createProgram(gl, vertexShader, fragmentShader);
+  if (!program) {
+    console.error("Couldn't load program");
+    return;
+  }
+
+  const vao = gl.createVertexArray();
+
+  render(gl, vao, program);
+}
+
+try {
+  main();
+} catch (e) {
+  console.error(e);
+}
